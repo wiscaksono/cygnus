d29a29c5abc74a85338ac9d3aabcb5ce72e29440
Wisnu Wicaksono <wwicaksono96@gmail.com>
2023-09-25 05:54:28 +0700

    add seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,42 @@
 import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker/locale/id_ID";
+import { RouterInputs } from "~/utils/api";
+
+type createPelamarInput = RouterInputs["pelamar"]["create"] & {
+  userId: string;
+};
+
 const prisma = new PrismaClient();
 
-(async () => {
-  await prisma.pelamar.deleteMany();
-  for (let i = 0; i < 100; i++) {
-    try {
-      await prisma.pelamar.create({
-        data: {
-          name: `Pelamar ${i}`,
-          email: `pelamar${i}@gmail.com`,
-          phone: `08123456789${i}`,
-          position: `Position ${i}`,
-          userId: "clmw0nxm40000inxavyn8ktw7",
-          interviewDate: new Date(),
-        },
-      });
-    } catch (error) {
-      await prisma.$disconnect();
-    } finally {
-      await prisma.$disconnect();
-    }
+async function main() {
+  await prisma.pelamar.deleteMany({});
+
+  const amountOfUsers = 10;
+  const pelamars: createPelamarInput[] = [];
+
+  for (let i = 0; i < amountOfUsers; i++) {
+    const pelamar: createPelamarInput = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      phone: faker.phone.number(),
+      position: faker.person.jobTitle(),
+      interviewDate: faker.date.future(),
+      userId: "clmxmmc3f0000in9gsok7j1dr",
+    };
+
+    pelamars.push(pelamar);
   }
-})();
+  const addUsers = async () =>
+    await prisma.pelamar.createMany({
+      data: pelamars,
+    });
+
+  addUsers();
+}
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
