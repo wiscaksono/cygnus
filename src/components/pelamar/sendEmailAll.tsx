11a4e180c03930e89b946754a6974d7870fc019e
Wisnu Wicaksono <wwicaksono96@gmail.com>
2023-10-04 16:07:40 +0700

    promise all send email

--- src/components/pelamar/sendEmailAll.tsx
+++ src/components/pelamar/sendEmailAll.tsx
@@ -19,15 +19,21 @@ export const SendEmailAll = ({ refetch, selectedPelamar }: ISendEmailAll) => {
   });
 
   const handleSend = async () => {
+    const promises = [];
+
     for (const person of selectedPelamar) {
-      await mutation.mutateAsync({
-        email: person.email,
-        interviewDate: person.interviewDate,
-        position: person.position,
-        namaPelamar: person.name,
-        portal: person.portal,
-      });
+      promises.push(
+        mutation.mutateAsync({
+          email: person.email,
+          interviewDate: person.interviewDate,
+          position: person.position,
+          namaPelamar: person.name,
+          portal: person.portal,
+        }),
+      );
     }
+
+    await Promise.all(promises);
     toast.success(`Mengirim undangan ke pelamar yang dipilih`);
   };
 
@@ -36,8 +42,7 @@ export const SendEmailAll = ({ refetch, selectedPelamar }: ISendEmailAll) => {
       type="button"
       className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm transition-opacity hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70"
       onClick={() => void handleSend()}
-      disabled={mutation.isLoading || selectedPelamar.some((item) => item.invitedByEmail === true)}
-    >
+      disabled={mutation.isLoading || selectedPelamar.some((item) => item.invitedByEmail === true)}>
       {mutation.isLoading ? "Mengirim..." : "Kirim Email"}
     </button>
   );
